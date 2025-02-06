2b2e0df0ad3eb37fa2d43c985ee4c4ff043fdbec
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-06 11:06:11 +0300

    Added new iOS target to the project
    Defined 'AppImageConstants' enum that will hold the name for the system images

---
+++ ProtonTodoDevTest/ProtonTodoDevTestiOS/Utilities/AppImageConsntants.swift
@@ -0,0 +1,14 @@
+//
+//  AppImageConsntants.swift
+//  ProtonTodoDevTestiOS
+//
+//  Created by Fenominall on 2/6/25.
+//
+
+import Foundation
+
+enum AppImageConsntants: String {
+    case house = "house"
+    case calendar = "calendar"
+    case bell = "bell"
+}
