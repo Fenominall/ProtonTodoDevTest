aa0f6a6cd9d6467cf6998802e357ebd8981c1c82
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-06 09:43:43 +0300

    Created 'URLSessionHTTPClient' class that conforms to the 'HTTPClient' and will be doing "GET" requests with the URLSession API. Handling Errors and for the correct response type as HTTPURLResponse

--- ProtonTodoDevTest/ProtonTodoDevTest/Shared API/URLSessionHTTPClient.swift
+++ ProtonTodoDevTest/ProtonTodoDevTest/Shared API/URLSessionHTTPClient.swift
@@ -7,3 +7,22 @@
 
 import Foundation
 
+public final class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    private struct UnexpectedValueRepresentaion: Error {}
+    
+    public func get(from url: URL) async throws -> HTTPResult {
+        let (data, response) = try await session.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw UnexpectedValueRepresentaion()
+        }
+        
+        return (data, httpResponse)
+    }
+}
