3074827e4db1ba4e1800241ab921cd1e087e18e8
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-06 10:35:49 +0300

    Add saving functionality for 'LocalTodoFeedManager'
    - Implemented TodoFeedCache conformance to save a feed of TodoItems to cache.
    - Added TodoItemSaveable conformance to save individual TodoItems to cache.
    - Introduced helper methods to convert TodoItems to LocalTodoItems for storage.
    - Ensured data is correctly saved by converting the models between their local and domain representations.
    
    Renamed the converting methods to clarify the intent adding ed

--- ProtonTodoDevTest/ProtonTodoDevTest/TodoFeedCache/LocalTodoFeedManager.swift
+++ ProtonTodoDevTest/ProtonTodoDevTest/TodoFeedCache/LocalTodoFeedManager.swift
@@ -15,17 +15,60 @@ public final class LocalTodoFeedManager {
     }
 }
 
+// MARK: - Loading Cache
 extension LocalTodoFeedManager: TodoFeedLoader {
     public func load() async throws -> [TodoItem] {
-        let items =  try await store.retrieve().convertToModels()
+        let items =  try await store.retrieve().convertedToModels()
         return items
     }
 }
 
+// MARK: - Saving to Cache
+extension LocalTodoFeedManager: TodoFeedCache {
+    public func save(_ feed: [TodoItem]) async throws {
+        try await store.insert(feed.convertedToLocale())
+    }
+}
+
+extension LocalTodoFeedManager: TodoItemSaveable {
+    public func save(_ item: TodoItem) async throws {
+        try await store.update(toLocal(for: item))
+    }
+    
+    private func toLocal(for item: TodoItem) -> LocalTodoItem {
+        LocalTodoItem(
+            id: item.id,
+            title: item.title,
+            description: item.description,
+            completed: item.completed,
+            createdAt: item.createdAt,
+            dueDate: item.dueDate,
+            imageURL: item.imageURL
+        )
+    }
+}
+
+// MARK: - Converting Helper Extensions
 private extension Array where Element == LocalTodoItem {
-    func convertToModels() -> [TodoItem] {
+    func convertedToModels() -> [TodoItem] {
         map {
             TodoItem(
+                id: $0.id,
+                title: $0.title,
+                description: $0.description,
+                completed: $0.completed,
+                createdAt: $0.createdAt,
+                dueDate: $0.dueDate,
+                imageURL: $0.imageURL
+            )
+        }
+    }
+}
+
+private extension Array where Element == TodoItem {
+    func convertedToLocale() -> [LocalTodoItem] {
+        map {
+            LocalTodoItem(
                 id: $0.id,
                 title: $0.title,
                 description: $0.description,
