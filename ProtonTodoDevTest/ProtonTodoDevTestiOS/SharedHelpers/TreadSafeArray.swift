a2935117355911f02cb49d0288b876d76fa49013
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-07 09:14:24 +0300

    feat: Implement thread-safe array with concurrent access handling

--- ProtonTodoDevTest/ProtonTodoDevTestiOS/SharedHelpers/TreadSafeArray.swift
+++ ProtonTodoDevTest/ProtonTodoDevTestiOS/SharedHelpers/TreadSafeArray.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+final class ThreadSafeArrat<Element> {
+    private var array: [Element] = []
+    private var queue = DispatchQueue(label: "swift.threadSafeDictionary.com", attributes: .concurrent)
+    
+    func append(_ eleemnt: Element) {
+        queue.async(flags: .barrier) {
+            self.array.append(eleemnt)
+        }
+    }
+    
+    func get(at index: Int) -> Element? {
+        queue.sync {
+            guard array.indices.contains(index) else { return nil }
+            return array[index]
+        }
+    }
+    
+    func update(at index: Int, with element: Element) {
+        queue.async(flags: .barrier) {
+            guard self.array.indices.contains(index) else { return }
+            self.array[index] = element
+        }
+    }
+    
+    func getAllElements() -> [Element] {
+        queue.sync {
+            return array
+        }
+    }
+    
+    var count: Int {
+        queue.sync {
+            return array.count
+        }
+    }
+}
