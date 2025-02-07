1b25498245db5da47af6680ab9ea507b1bfcc81f
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-07 14:46:29 +0300

    Provided implementation for the TodoFeedImageStore in CoreDataFeedStore to store and retrieve binary data from teh CoreData

--- ProtonTodoDevTest/ProtonTodoDevTest/TodoFeedCache/Infrastructure/CoreData/CoreDataFeedStore+TodoFeedimageStore.swift
+++ ProtonTodoDevTest/ProtonTodoDevTest/TodoFeedCache/Infrastructure/CoreData/CoreDataFeedStore+TodoFeedimageStore.swift
@@ -9,11 +9,16 @@ import Foundation
 
 extension CoreDataFeedStore: TodoFeedImageStore {
     public func cache(_ data: Data, for url: URL) async throws {
-        // TODO
+        try await performAsync { context in
+            try ManagedTodoItem.first(with: url, in: context)
+                .map { $0.data = data }
+                .map(context.save)
+        }
     }
     
     public func retrieve(from url: URL) async throws -> Data? {
-        // TODO
-        Data()
+        try await performAsync { context in
+            return try ManagedTodoItem.data(with: url, in: context)
+        }
     }
 }
