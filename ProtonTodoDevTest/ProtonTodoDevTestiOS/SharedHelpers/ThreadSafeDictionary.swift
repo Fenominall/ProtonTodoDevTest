e354d409e7dfee1c1a6f1f490fa1c03826cf5e45
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-07 08:55:56 +0300

    Added ThreadSafeDictionary for safe muthreading operations with the dictionary

--- ProtonTodoDevTest/ProtonTodoDevTestiOS/SharedHelpers/ThreadSafeDictionary.swift
+++ ProtonTodoDevTest/ProtonTodoDevTestiOS/SharedHelpers/ThreadSafeDictionary.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+final class ThreadSafeDictionary<Key: Hashable, Value> {
+    
+    private var dictionary = [Key: Value]()
+    private var queue = DispatchQueue(label: "swift.threadSafeDictionary.com", attributes: .concurrent)
+    
+    subscript(key: Key) -> Value? {
+        get {
+            var value: Value?
+            queue.sync {
+                value = dictionary[key]
+            }
+            return value
+        }
+        set(newValue) {
+            queue.async(flags:. barrier) { [weak self] in
+                self?.dictionary[key] = newValue
+            }
+        }
+    }
+}
