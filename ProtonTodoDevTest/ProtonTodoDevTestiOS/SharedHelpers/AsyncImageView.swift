803d9671101bb486c2ae3344f47ecb2ead1b35a1
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-09 09:36:11 +0300

    Introduced 'AsyncimageView' that received data and asyncronously converts it into the UIImage then assiging to the Image view otherwise processing, shwoing ProgressView until finished Using the introduced 'AsyncImageView' in the 'TaskRoewImageView' for image rendering with the received data

--- ProtonTodoDevTest/ProtonTodoDevTestiOS/SharedHelpers/AsyncImageView.swift
+++ ProtonTodoDevTest/ProtonTodoDevTestiOS/SharedHelpers/AsyncImageView.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct AsyncImageView: View {
+    let imageData: Data
+    @State private var uiImage: UIImage?
+    
+    var body: some View {
+        Group {
+            if let uiImage {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .clipped()
+            } else {
+                ProgressView()
+                    .onAppear {
+                        Task {
+                            let data = await processImageData(imageData)
+                            await MainActor.run { uiImage = data }
+                        }
+                    }
+            }
+        }
+    }
+    
+    private func processImageData(_ data: Data) async -> UIImage? {
+        await Task.detached(priority: .userInitiated) {
+            UIImage(data: data)
+        }.value
+    }
+}
--- ProtonTodoDevTest/ProtonTodoDevTestiOS/Todo Feed UI/Views/TaskListViews/TaskRowViewComponents/TaskRowImageView.swift
+++ ProtonTodoDevTest/ProtonTodoDevTestiOS/Todo Feed UI/Views/TaskListViews/TaskRowViewComponents/TaskRowImageView.swift
@@ -8,21 +8,21 @@
 import SwiftUI
 
 struct TaskRowImageView: View {
-    let imageData: Data
+    let imageData: Data?
     private let iconSize: CGFloat = 50
     
     var body: some View {
-        if let uiImage = UIImage(data: imageData) {
-            Image(uiImage: uiImage)
-                .resizable()
-                .scaledToFit()
-                .frame(width: iconSize, height: iconSize)
-                .padding([.leading, .trailing], 15)
-                .padding(.bottom, 10)
-        } else {
-            Rectangle()
-                .foregroundStyle(.gray)
-                .frame(width: iconSize, height: iconSize)
+        Group {
+            if let data = imageData, !data.isEmpty {
+                AsyncImageView(imageData: data)
+            } else {
+                Rectangle()
+                    .foregroundStyle(.gray)
+            }
         }
+        .frame(width: iconSize, height: iconSize)
+        .padding([.leading, .trailing], 15)
+        .padding(.bottom, 10)
     }
 }
+
