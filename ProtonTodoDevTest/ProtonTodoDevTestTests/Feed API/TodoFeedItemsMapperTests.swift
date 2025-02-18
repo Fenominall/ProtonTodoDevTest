fd3e5acba92e1b5000663d167c83e9f22ad432fe
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-18 09:54:57 +0300

    Added new tests to TodoFeedItemsMapper - throws Error On Invalid JSON And 200 StatusCode - throws Error On Invalid Date Format - throws Error On Non UUID Dependencies Array - delivers No Items On 200 HTTpResponse WithEmpty List
    Moved some helper functions to a shared scope in the test target

--- ProtonTodoDevTest/ProtonTodoDevTestTests/Feed API/TodoFeedItemsMapperTests.swift
+++ ProtonTodoDevTest/ProtonTodoDevTestTests/Feed API/TodoFeedItemsMapperTests.swift
@@ -9,39 +9,84 @@ import XCTest
 import ProtonTodoDevTest
 
 class TodoFeedItemsMapperTests: XCTestCase {
+    func test_map_throwsErrorOnInvalidJSONAnd200StatusCode() throws {
+        let invalidJson = Data("invalidJson".utf8)
+        
+        XCTAssertThrowsError(
+            try TodoFeedItemsMapper.map(
+                invalidJson,
+                from: HTTPURLResponse(statusCode: 200)
+            )
+        )
+    }
     
-    // MARK: - Helpers
-    private func makeItem(
-        id: UUID,
-        title: String,
-        description: String,
-        completed: Bool,
-        createdAt: Date,
-        dueDate: Date,
-        imageURL: URL,
-        dependencies: [UUID]
-    ) -> (model: TodoItem, json: [String: Any]) {
-        let item = TodoItem(
-            id: id,
-            title: title,
-            description: description,
-            completed: completed,
-            createdAt: createdAt,
-            dueDate: dueDate,
-            imageURL: imageURL,
-            dependencies: dependencies
+    func test_map_throwsErrorOnInvalidDateFormat() throws {
+        let invalidDateJson: [[String: Any]] = [
+            [
+                "id": UUID().uuidString,
+                "title": "A title",
+                "description": "A description",
+                "completed": false,
+                "createdAt": "invalid-date",
+                "dueDate": Date().iso8601FormattedString(),
+                "imageURL": "https://any-url.com",
+                "dependencies": []
+            ]
+        ]
+        
+        let json = try makeItemsJSON(invalidDateJson)
+        
+        XCTAssertThrowsError(
+            try TodoFeedItemsMapper.map(
+                json,
+                from: HTTPURLResponse(statusCode: 200)
+            )
+        )
+    }
+    
+    func test_map_throwsErrorOnNonUUIDDependenciesArray() throws {
+        let invalidDateJson: [[String: Any]] = [
+            [
+                "id": UUID().uuidString,
+                "title": "A title",
+                "description": "A description",
+                "completed": false,
+                "createdAt": Date().iso8601FormattedString(),
+                "dueDate": Date().iso8601FormattedString(),
+                "imageURL": "https://any-url.com",
+                "dependencies": ["invalid-id"]
+            ]
+        ]
+        
+        let json = try makeItemsJSON(invalidDateJson)
+        
+        XCTAssertThrowsError(
+            try TodoFeedItemsMapper.map(
+                json,
+                from: HTTPURLResponse(statusCode: 200)
+            )
+        )
+    }
+    
+    // MARK: - Success case
+    func test_map_deliversNoItemsOn200HTTpResponseWithEmptyList() throws {
+        let emptyJson = try makeItemsJSON([])
+        
+        let result = try TodoFeedItemsMapper.map(
+            emptyJson,
+            from: HTTPURLResponse(statusCode: 200)
         )
         
-        let json = [
-            "id": id.uuidString,
-            "title": title,
-            "description": description,
-            "completed": completed,
-            "createdAt": createdAt,
-            "dueDate": dueDate,
-            "imageURL": imageURL.absoluteString,
-            "dependencies": dependencies.map { $0.uuidString }
-        ].compactMapValues { $0 }
-        return (item, json )
+        XCTAssertEqual(result, [])
+    }
+}
+
+extension HTTPURLResponse {
+    convenience init(statusCode: Int) {
+        self.init(
+            url: anyURL(),
+            statusCode: statusCode,
+            httpVersion: nil,
+            headerFields: nil)!
     }
 }
