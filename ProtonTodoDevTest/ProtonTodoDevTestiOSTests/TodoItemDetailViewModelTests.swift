ae38b2686f17ae7fbfc6255c98a6fc9037d49869
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-20 16:02:04 +0300

    add[test] - Added new 'TodoItemDetailViewModelTests'
    Did add helper maskeSUT factory method

--- ProtonTodoDevTest/ProtonTodoDevTestiOSTests/TodoItemDetailViewModelTests.swift
+++ ProtonTodoDevTest/ProtonTodoDevTestiOSTests/TodoItemDetailViewModelTests.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Combine
+import XCTest
+import ProtonTodoDevTest
+@testable import ProtonTodoDevTestiOS
+
+final class TodoItemDetailViewModelTests: XCTestCase {
+    
+    // MARK: - Helpers
+    private func makeSUT(
+        task: TodoItem = uniqueItem(),
+        imageLoad: @escaping () async throws -> Data? = {
+            Data()
+        },
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> TodoItemDetailViewModel {
+        let sut = TodoItemDetailViewModel(
+            task: task,
+            imageLoad: imageLoad
+        )
+        trackForMemoryLeaks(
+            sut,
+            file: file,
+            line: line
+        )
+        return sut
+    }
+}
