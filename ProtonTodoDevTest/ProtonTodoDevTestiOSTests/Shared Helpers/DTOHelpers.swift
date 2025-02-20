b775e5b0d282e413134d618418605f1484c19be3
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-20 17:54:06 +0300

    add[test] - added helper factory method to create a TodoItemPresentationModel in the ProtonTodoDevTestiOS test target

--- ProtonTodoDevTest/ProtonTodoDevTestiOSTests/Shared Helpers/DTOHelpers.swift
+++ ProtonTodoDevTest/ProtonTodoDevTestiOSTests/Shared Helpers/DTOHelpers.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+@testable import ProtonTodoDevTestiOS
+
+func anyPresentationModel(
+    id: UUID = UUID(),
+    title: String = "Task",
+    description: String = "Description",
+    completed: Bool = false,
+    createdAt: Date = Date(),
+    dueDate: Date = Date().addingTimeInterval(86400),
+    imageURL: URL = URL(string: "https://example.com")!,
+    imageData: Data? = nil,
+    dependencies: [UUID] = []
+) -> TodoItemPresentationModel {
+    TodoItemPresentationModel(
+        id: id,
+        title: title,
+        description: description,
+        completed: completed,
+        createdAt: createdAt,
+        dueDate: dueDate,
+        imageURL: imageURL,
+        imageData: imageData,
+        dependencies: dependencies
+    )
+}
