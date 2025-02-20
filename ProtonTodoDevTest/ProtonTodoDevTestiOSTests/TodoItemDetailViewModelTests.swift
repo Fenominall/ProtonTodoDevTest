ebc302f06d3a220f8d1f17ae30f1de2305f76839
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-20 16:03:23 +0300

    add[test] - Added init test to the 'TodoItemDetailViewModelTests' that makes sue that 'TodoItemDetailViewModel' receives correct values on creation

--- ProtonTodoDevTest/ProtonTodoDevTestiOSTests/TodoItemDetailViewModelTests.swift
+++ ProtonTodoDevTest/ProtonTodoDevTestiOSTests/TodoItemDetailViewModelTests.swift
@@ -12,6 +12,32 @@ import ProtonTodoDevTest
 @testable import ProtonTodoDevTestiOS
 
 final class TodoItemDetailViewModelTests: XCTestCase {
+    private var cancelables = Set<AnyCancellable>()
+    
+    override func tearDown() {
+        cancelables.removeAll()
+        super.tearDown()
+    }
+    
+    func test_init_setsCorrectInitialValues() {
+        let item = uniqueItem(
+            title: "Custom Title",
+            description: "Custom Description",
+            createdAt: Date(timeIntervalSince1970: 0),
+            dueDate: Date(timeIntervalSince1970: 86400)
+        )
+        
+        let sut = makeSUT(task: item)
+        
+        XCTAssertEqual(sut.title, item.title)
+        XCTAssertEqual(sut.description, item.description)
+        XCTAssertEqual(sut.createdAt, item.createdAt.iso8601FormattedString())
+        XCTAssertEqual(sut.dueDate, item.dueDate.iso8601FormattedString())
+        XCTAssertNil(sut.imageData)
+        XCTAssertFalse(sut.isImageLoading)
+        XCTAssertFalse(sut.showImageLoadingError)
+        XCTAssertNil(sut.imageLoadingError)
+    }
     
     // MARK: - Helpers
     private func makeSUT(
