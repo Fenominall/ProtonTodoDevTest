576a76fda6487fd6ebbfb23c1ed7714626d4cf9c
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-20 17:55:26 +0300

    add[test] - Introduced TaskRoewViewModelTests Did add makeSUT factory helper method for SUT creation

--- ProtonTodoDevTest/ProtonTodoDevTestiOSTests/TodoItemDetail/Presentation/TaskRowViewModelTests.swift
+++ ProtonTodoDevTest/ProtonTodoDevTestiOSTests/TodoItemDetail/Presentation/TaskRowViewModelTests.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import Combine
+import ProtonTodoDevTest
+@testable import ProtonTodoDevTestiOS
+import SwiftUI
+import XCTest
+
+final class TaskRowViewModelTests: XCTestCase {
+
+    // MARK: - Helpers
+    private func makeSUT(
+        task: TodoItemPresentationModel = anyPresentationModel(),
+        imageLoad: @escaping () async throws -> Data? = {
+            Data()
+        },
+        taskID: @escaping (UUID) async -> Bool = {
+            _ in true
+        },
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> TaskRowViewModel {
+        
+        var localTask = task
+        let bindingtask = Binding<TodoItemPresentationModel>(
+            get: { localTask },
+            set: { localTask = $0 }
+        )
+        
+        let sut = TaskRowViewModel(
+            receivedTask: bindingtask,
+            loadImageData: imageLoad,
+            taskId: taskID
+        )
+        trackForMemoryLeaks(
+            sut,
+            file: file,
+            line: line
+        )
+        return sut
+    }
+}
