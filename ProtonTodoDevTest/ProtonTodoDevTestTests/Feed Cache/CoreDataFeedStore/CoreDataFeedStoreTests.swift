6cc5f3ceaa6126601d8cec2ec446e73c23fcea00
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-20 10:34:21 +0300

    add[test] - Did create new test group for CoreDataFeedStoreTests to cover CoreDataFeedStore edge cases

---
+++ ProtonTodoDevTest/ProtonTodoDevTestTests/Feed Cache/CoreDataFeedStore/CoreDataFeedStoreTests.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  ProtonTodoDevTestTests
+//
+//  Created by Fenominall on 2/20/25.
+//
+
+import Foundation
+import XCTest
+import ProtonTodoDevTest
+
+final class CoreDataFeedStoreTests: XCTestCase {
+    
+}
