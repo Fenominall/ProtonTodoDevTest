b4704873ae99a99fe011d4f53f71777863904970
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-20 10:38:09 +0300

    Feat[test] - Did make 'CoreDataFeedStoreSpecs' conform to the CoreDataFeedStoreSpecs typealis including - RetrieveStoreSpecs & FailableRetrieveStoreSpecs & InsertStoreSpecs & FailableInsertStoreSpecs & UpdateStoreSpecs & FailableUpdateStoreSpecs

--- ProtonTodoDevTest/ProtonTodoDevTestTests/Feed Cache/CoreDataFeedStore/CoreDataFeedStoreTests.swift
+++ ProtonTodoDevTest/ProtonTodoDevTestTests/Feed Cache/CoreDataFeedStore/CoreDataFeedStoreTests.swift
@@ -9,6 +9,70 @@ import Foundation
 import XCTest
 import ProtonTodoDevTest
 
-final class CoreDataFeedStoreTests: XCTestCase {
+typealias CoreDataFeedStoreSpecs = RetrieveStoreSpecs & FailableRetrieveStoreSpecs & InsertStoreSpecs & FailableInsertStoreSpecs & UpdateStoreSpecs & FailableUpdateStoreSpecs
+
+final class CoreDataFeedStoreTests: XCTestCase, CoreDataFeedStoreSpecs  {
+    func test_retrieve_deliversEmptyOnEmptyStore() {
+        
+    }
     
+    func test_retrieve_hasNoSideEffectsOnEmptyStore() {
+        
+    }
+    
+    func test_retrieve_deliversDataOnNonEmptyStore() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyStore() {
+        
+    }
+    
+    func test_retrieve_deliversFailureOnError() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnFailure() {
+        
+    }
+    
+    func test_insert_succeedsOnEmptyStore() {
+        
+    }
+    
+    func test_insert_succeedsOnNonEmptyStore() {
+        
+    }
+    
+    func test_insert_overridesExistingData() {
+        
+    }
+    
+    func test_insert_deliversFailureOnError() {
+        
+    }
+    
+    func test_insert_hasNoSideEffectsOnFailure() {
+        
+    }
+    
+    func test_update_succeedsOnNonEmptyStore() {
+        
+    }
+    
+    func test_update_modifiesExistingData() {
+        
+    }
+    
+    func test_update_hasNoEffectOnEmptyStore() {
+        
+    }
+    
+    func test_update_deliversFailureOnError() {
+        
+    }
+    
+    func test_update_hasNoSideEffectsOnFailure() {
+        
+    }
 }
