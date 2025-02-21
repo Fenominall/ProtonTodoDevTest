16df0966f2888bb8997b42299768ebfca87c4279
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-21 10:28:44 +0300

    add/update[chore] - Added missing test schemes, updated exsting ones Assigned real httpClient for a FeedLoader use

--- ProtonTodoDevTestApp/ProtonTodoDevTestApp/ProtonTodoDevTestAppApp.swift
+++ ProtonTodoDevTestApp/ProtonTodoDevTestApp/ProtonTodoDevTestAppApp.swift
@@ -82,7 +82,7 @@ struct ProtonTodoDevTestAppApp: App {
     }
     
     private func makeRemoteFeedLoader() -> AnyPublisher<[TodoItem], Error> {
-        return httpClient
+        return mockHttpClient
             .getPublisher(from: baseURL)
             .tryMap(TodoFeedItemsMapper.map)
             .eraseToAnyPublisher()
