5825808e0f0adeb784a79d41854667541da0d85c
Vladislav Todorov <it.vtodorov@gmail.com>
2025-02-21 08:06:32 +0300

    add[test] - TasksFilteringManager return sorted task by creation date in the descending order predicate

--- ProtonTodoDevTestApp/ProtonTodoDevTestAppTests/TasksFilteringManagerTests.swift
+++ ProtonTodoDevTestApp/ProtonTodoDevTestAppTests/TasksFilteringManagerTests.swift
@@ -22,4 +22,18 @@ final class TasksFilteringManagerTests: XCTestCase {
         let emptySortedTasks = TasksFilteringManager.sortTasksBy([anyTask]) { $0.createdAt > $1.createdAt }
         XCTAssertEqual(emptySortedTasks, [anyTask])
     }
+    
+    func test_sortedTasksBy_returnsUnsortedTasksAsSortedTasksByCreationDateInTheDescendingOrderPredicate() {
+        let date1 = Date(timeIntervalSince1970: 1000)
+        let date2 = Date(timeIntervalSince1970: 0)
+        let task1 = uniqueItem(createdAt: date2)
+        let task2 = uniqueItem(createdAt: date1)
+        
+        let unsortedTasks = [task1, task2]
+        let sortedTasks = TasksFilteringManager.sortTasksBy(unsortedTasks) { $0.createdAt > $1.createdAt }
+        
+        XCTAssertEqual(sortedTasks.count, 2)
+        XCTAssertEqual(sortedTasks[0].createdAt, date1)
+        XCTAssertEqual(sortedTasks[1].createdAt, date2)
+    }
 }
